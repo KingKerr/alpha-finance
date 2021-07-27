07081e3f0c5484fecd15dc9f83fd565b9784ce98
Drew Kerr <kingkerr@Drews-MacBook-Pro.local>
2021-07-26 19:53:14 -0500

    Added tokens securely. Also able to lookup Stock prices.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,10 @@
 class Stock < ApplicationRecord
+  def self.new_lookup(ticker)
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.IEX_Client[:sandbox_api_key],
+      secret_token: Rails.application.credentials.IEX_Client[:sandbox_secret_key],
+      endpoint: 'https://sandbox.iexapis.com/v1'
+    )
+    client.price(ticker)
+  end  
 end
